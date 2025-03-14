a0cdd2a4c03ccb5f73d3170a9377d8a95fabcfc2
Marina_Pasternak <256pasternakmarina@gmail.com>
2025-03-14 21:21:06 +0200

    fixed DatePicker onSelect Error

--- src/locations/PublishedDateField.tsx
+++ src/locations/PublishedDateField.tsx
@@ -1,32 +1,50 @@
 import { useEffect, useState } from "react";
 import { useSDK } from "@contentful/react-apps-toolkit";
 import { FieldAppSDK } from "@contentful/app-sdk";
+
+import { Flex, Box } from '@contentful/f36-components';
 import { Datepicker } from "@contentful/f36-components";
 
 const PublishedDateField = () => {
   const sdk = useSDK<FieldAppSDK>();
-  const [date, setDate] = useState<string>("");
+  const [date, setDate] = useState<Date | undefined>(new Date());
   
   useEffect(() => {
-    sdk.window.updateHeight(420);
-    const initialDate = sdk.field.getValue() as string | undefined;
+    const initialDate = new Date(sdk.field.getValue()) as Date | undefined;
 
     if (!initialDate) {
-      const today = new Date().toISOString().split("T")[0];
+      const today = new Date();
       sdk.field.setValue(today);
       setDate(today);
     } else {
       setDate(initialDate);
     }
   }, []);
+
+  useEffect(() => {
+    sdk.window.startAutoResizer({ absoluteElements: true });
+  });
   
   return (
-    <Datepicker
-      dateFormat="dd.MM.yyyy"
-      selected={date ? new Date(date) : undefined}
-      onSelect={setDate}
-      style={{ width: "250px" }}
-    />
+      <Flex 
+        marginBottom="spacingM" 
+        flexDirection="column" 
+      >
+      <Box 
+        marginBottom="spacingM"
+        style={{
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        <Datepicker
+            dateFormat="dd.MM.yyyy"
+            selected={date ? new Date(date) : undefined}
+            onSelect={setDate}
+            style={{ width: "250px" }}
+        />
+      </Box>
+    </Flex>
   );
 };
 
